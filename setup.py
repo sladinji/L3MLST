f67de5abd612ceeb8fac7ba2a6cf58a42b6fcafd
2b2s <julien.almarcha@univ-fcomte.fr>
2020-09-24 11:11:53 +0200

    coockie cutter bootstrap

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Réalisation d’une analyse phylogénitique : le data engineering en biologie',
+    author='Julien Almarcha - UFR Santé',
+    license='MIT',
+)
